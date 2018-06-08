98548e0b662c740ce74a1ddbbb5efce6f20cca19
51m0n2063r <simon.roger31@gmail.com>
2018-06-08 13:40:36 +0200

    mise en place html css p5js

---
+++ monpaintP5/app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+httpServer = http.createServer(function (req, res) {
+    res.writeHead(200);
+    console.log('un utilisateur a afficher la page');
+});
+var server = http.createServer('index.html');
+
+
+
+
+function setup() {
+  createCanvas(1200, 650);
+  background(252,252,252);
+
+}
+
+function draw() {
+  stroke(90);
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+httpServer.listen(1337);
